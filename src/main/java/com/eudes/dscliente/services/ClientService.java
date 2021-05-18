25334eeb4e8241c8b9eff913ea7ea209edf1665a
Eudes Morais <eudes.morais@gmail.com>
2021-05-18 01:04:03 -0300

    Delete client

--- src/main/java/com/eudes/dscliente/services/ClientService.java
+++ src/main/java/com/eudes/dscliente/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.eudes.dscliente.dto.ClientDTO;
 import com.eudes.dscliente.entities.Client;
 import com.eudes.dscliente.repositories.ClientRepository;
+import com.eudes.dscliente.services.exceptions.DatabaseException;
 import com.eudes.dscliente.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -72,6 +75,21 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id not found: " + id);
 		}
 	}
-	
-	
+
+	public void delete(Long id) {
+		
+		try {
+			
+			repository.deleteById(id);
+				
+		} catch (EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException("Id not found: " + id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DatabaseException("Integrity violation");
+			
+		}
+	}	
 }
